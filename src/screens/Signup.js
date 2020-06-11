1260fec9e44e43cee6f15c54dcc573f0330894cb
chad waggoner <chad.waggoner@gmail.com>
2020-06-10 17:55:24 -0800

    login and signup

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -4,6 +4,14 @@ const Signup = () =>
 (
     <div> 
         <h1>Signup Page</h1>
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required></input>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
     </div>
 )
 
