84f9587b740dc79266a9b931e34898b891d45610
ElleRose <lindseyurquhart@tutanota.com>
2020-06-29 17:54:19 -0400

    working on adding churchmarkers added Map2.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,93 +1,80 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { listChurches } from '../components/Api';
+import "mapbox-gl/dist/mapbox-gl.css"
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css"
+import React from 'react'
+import MapGL from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
+
+class Map extends React.Component { 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 41.07963180541992,
+        longitude: -85.13732147216797,
+        zoom: 10
+      },
+      searchResultLayer: null
+    }
+  }
+
+  mapRef = React.createRef()
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  };
 
 
+  handleOnResult = event => {
+    this.setState({
+      searchResultLayer: new GeoJsonLayer({
+        id: "search-result",
+        data: event.result.geometry,
+        getFillColor: [255, 0, 0, 128],
+        getRadius: 1000,
+        pointRadiusMinPixels: 10,
+        pointRadiusMaxPixels: 10,
+      })      
+    })
+  }
 
-
-
-function Map() {
-  const [churches, setChurches] = useState([]);
-  const [viewport, setViewport] = useState({
-    longitude: -86.1746933,
-    latitude: 40.3270127,
-    width: '60vw',
-    height: '70vh',
-    zoom: 6
-  });
-
-  const mapRef = useRef();
-
-
-    useEffect(() => {
-    (async () => {
-      const churches = await listChurches();
-      setChurches(churches);
-      console.log(churches);
-    })();
-
-    return () => {
-      // clean up things ...
-      // is like a componentUnmount()
-    };
-  }, []);
-
-  return (
-    <div >
-      <ReactMapGL
-        className='mapContainer'
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        maxZoom={20}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onViewportChange={(newViewport) => {
-          setViewport({ ...newViewport });
-        }}
-        ref={mapRef}
-      >
-        
-        {
-
-        churches.map(church => (
-          <Marker 
-            key={church.id}
-            // latitude={41.0799898}
-            longitude={parseFloat(church.Longitude)}
-            latitude={parseFloat(church.Latitude)}
-            // longitude={-85.1386015}
-            
-            offsetLeft={-20}
-            offsetTop={-10}>
-            <div>
-              <h1>Church</h1>
-            <button className='marker-btn'>
-              <img src='/colorchurch.svg' alt='church' />
-            </button>
-          </div>  
-
-          </Marker>
-
-        ))
-      }
-
-        {/* <Marker
-          latitude={41.07776110}
-          longitude={-85.14752470}
+  render() {
+    const { viewport, searchResultLayer } = this.state
+    return (
+      <div style={{ height: '100vh' }}>
+        <MapGL className="mapContainer"
+          ref={this.mapRef}
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          width="50%"
+          height="70%"
+          onViewportChange={this.handleViewportChange}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         >
-          <div>
-            <button className='marker-btn'>
-              <img src='/colorchurch.svg' alt='church' />
-            </button>
-          </div>          
-        </Marker> */}
-
-      </ReactMapGL>
-
-    </div>
-
-  )
+          <Geocoder
+            mapRef={this.mapRef}
+            onResult={this.handleOnResult}
+            onViewportChange={this.handleGeocoderViewportChange}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            position='top-left'
+          />
+        </MapGL>
+        <DeckGL {...viewport} layers={[searchResultLayer]} />
+      </div>
+    )
+  }
 }
 
 export default Map;
---
+++ src/components/Map2.js
@@ -0,0 +1,93 @@
+import React, { useState, useRef, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { listChurches } from './Api';
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css"
+
+
+
+
+
+function Map() {
+  const [churches, setChurches] = useState([]);
+  const [viewport, setViewport] = useState({
+    longitude: -86.1746933,
+    latitude: 40.3270127,
+    width: '60vw',
+    height: '70vh',
+    zoom: 6
+  });
+
+  const mapRef = useRef();
+
+
+    useEffect(() => {
+    (async () => {
+      const churches = await listChurches();
+      setChurches(churches);
+      console.log(churches);
+    })();
+
+    return () => {
+      // clean up things ...
+      // is like a componentUnmount()
+    };
+  }, []);
+
+  return (
+    <div >
+      <ReactMapGL
+        className='mapContainer'
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        maxZoom={20}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(newViewport) => {
+          setViewport({ ...newViewport });
+        }}
+        ref={mapRef}
+      >
+        
+        {
+
+        churches.map(church => (
+          <Marker 
+            key={church.id}
+            // latitude={41.0799898}
+            longitude={parseFloat(church.Longitude)}
+            latitude={parseFloat(church.Latitude)}
+            // longitude={-85.1386015}
+            
+            offsetLeft={-20}
+            offsetTop={-10}>
+            <div>
+              <h1>Church</h1>
+            <button className='marker-btn'>
+              <img src='/colorchurch.svg' alt='church' />
+            </button>
+          </div>  
+
+          </Marker>
+
+        ))
+      }
+
+        {/* <Marker
+          latitude={41.07776110}
+          longitude={-85.14752470}
+        >
+          <div>
+            <button className='marker-btn'>
+              <img src='/colorchurch.svg' alt='church' />
+            </button>
+          </div>          
+        </Marker> */}
+
+      </ReactMapGL>
+
+    </div>
+
+  )
+}
+
+export default Map;
