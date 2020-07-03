1dbf4c69c52a24194ba37fdf3f1e952a3d46f7f6
ElleRose <lindseyurquhart@tutanota.com>
2020-07-03 18:27:09 -0400

    updated homepage and added FindChurch screen

---
+++ src/screens/Findchurch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map2 from '../components/Map2';
+
+const Findchurch = () => (
+    <div>
+        <Map2 />
+    </div>
+);
+
+export default Findchurch;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import Map2 from '../components/Map2';
+import { Link } from 'react-router-dom';
+
+
 
 
 const Home = () => (
-    <div>
-    <Map2 />
+    <div id='background-container'>
+        <img id='home-background-img' src='background.jpg' alt='homepage-background'></img>
+        <div className='home-text'>
+            <h1>Find Your Home Church</h1>
+            <br></br>
+            <div>
+            <button onClick={e => window.location.href = '/Findchurch'}>Find A Church</button> 
+
+            <button onClick={e => window.location.href = '/Addchurch'}>Add Your Church</button>
+            </div>
+        </div>
+
     </div>
- 
-  
+
+
 );
 
 export default Home;
- 
