433e9bcd4438a01a25094aa663093091a0be70d3
ElleRose <lindseyurquhart@tutanota.com>
2020-07-08 14:43:58 -0400

    buttons and homepage title responsive

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,15 +8,15 @@ const Home = () => (
     <div id='background-container'>
         <img id='home-background-img' src='background.jpg' alt='homepage-background'></img>
         <div className='home-text'>
-            
-            <h1>Find Your Home Church</h1>
+
+            <h1 className='homepage-title'>Find Your Home Church</h1>
             <br></br>
             <div>
-            <button onClick={e => window.location.href = '/Findchurch'}>Find A Church</button> 
+                <button onClick={e => window.location.href = '/Findchurch'}>Find A Church</button>
 
-            <button onClick={e => window.location.href = '/Addchurch'}>Add Your Church</button>
+                <button onClick={e => window.location.href = '/Addchurch'}>Add Your Church</button>
             </div>
-           
+
         </div>
 
     </div>
