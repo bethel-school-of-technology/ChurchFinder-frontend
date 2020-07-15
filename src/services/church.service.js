ccd4a3ecc605a65e2a5769d5f80e1eb75057f040
ElleRose <lindseyurquhart@tutanota.com>
2020-07-14 21:20:32 -0400

    not sure

--- src/services/church.service.js
+++ src/services/church.service.js
@@ -5,6 +5,8 @@ class ChurchDataService {
         return http.get('/churches/churchlist');
     }
 
+    
+//working on this to find churches by denomination
     findByDenomination(Denomination) {
         return http.get(`/churches/search/denomination?denomination=${Denomination}`);
     }
